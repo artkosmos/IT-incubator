15436cf5787445fbbfc6c0c1becf1d028f1b5ecc
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-04-30 18:47:01 +0300

    initialization

---
+++ training/src/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="Header-menu">
+      <a href="#">Home</a>
+      <a href="#">News Feed</a>
+      <a href="#">Messages</a>
+    </div>
+  )
+}
+
+export default Header;
---
+++ training/src/Technologies.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Technologies = () => {
+  return (
+    <div className="Technologies-main">
+      <h1>Hello, samurai! Let's go !</h1>
+      <ol>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+      </ol>
+    </div>
+  )
+}
+
+export default Technologies;
