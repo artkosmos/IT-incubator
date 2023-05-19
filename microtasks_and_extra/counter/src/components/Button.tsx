3c85bd70acde869cf68a8631ad652bf75b837b00
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-05-19 21:39:50 +0300

    implement simple counter until five

---
+++ microtasks_and_extra/counter/src/components/Button.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import style from './Button.module.css'
+
+type ButtonPropsType = {
+  children: React.ReactNode
+  callBack: () => void
+  disabled?: boolean
+}
+
+export const Button: React.FC<ButtonPropsType> = ({callBack, children, disabled}) => {
+
+  /*const onClickHandler = () => {
+    callBack()
+  }*/
+
+  return (
+    <button
+      className={disabled ? `${style.button} ${style.disable}` : `${style.button}`}
+      onClick={callBack}
+      disabled={disabled}
+    >{children}
+    </button>
+  )
+}
