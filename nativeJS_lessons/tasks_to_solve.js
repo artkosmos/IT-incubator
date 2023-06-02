ea0273e3dfec0048d64bd28ff9f0f7c24f119aa1
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-06-02 22:42:05 +0300

    start task native JS

---
+++ nativeJS_lessons/tasks_to_solve.js
@@ -0,0 +1,132 @@
+const students = [
+    {
+        id: 1,
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85,
+        // к 14 задаче: friends: ["Alex", "Nick", "John", "Helen", "Ann"]
+    },
+    {
+        id: 2,
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 90,
+    },
+    {
+        id: 3,
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },
+    {
+        id: 4,
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 100
+    },
+    {
+        id: 5,
+        name: "Helen",
+        age: 20,
+        isMarried: false,
+        scores: 110
+    },
+    {
+        id: 6,
+        name: "Ann",
+        age: 20,
+        isMarried: false,
+        scores: 105
+    },
+];
+
+const user = {
+    name: "Bob",
+    age: 23,
+    friends: ["Alex", "Nick", "John"]
+}
+
+const superUser = {
+    name: "Bob",
+    age: 23,
+    friends: [
+        {
+            id: 1,
+            name: "Ann",
+            age: 22,
+            isMarried: true,
+            scores: 85
+        },
+        {
+            id: 2,
+            name: "Alex",
+            age: 21,
+            isMarried: true,
+            scores: 90,
+        },
+        {
+            id: 4,
+            name: "John",
+            age: 19,
+            isMarried: false,
+            scores: 100
+        }
+    ]
+}
+// NB!!! Все преобразования выполняем иммьютабельно, если не сказано иное
+
+//1. Создайте полную (глубокую) копию объекта user ++++
+
+//2. Создайте полную (глубокую) массива students ++++
+
+//3. Создайте полную (глубокую) копию объекта superUser
+
+//4. Отсортируйте students по успеваемости (лучший идёт первым)(sort)
+
+//5. Сформируйте массив студентов, у которых 100 и более баллов (filter)
+
+//6. Сформируйте массив имён студентов (map)
+
+//7. Добавьте всем студентам свойство "isStudent" со значением true (map)
+
+//8. Nick женился. Выполните соответствующие преобразование массива
+// students (map)
+
+// Внесите  следующие изменения в объект superUser:
+// NB!!! Все преобразования выполняем иммьютабельно, если не сказано иное
+
+//9.Удалите объект с id=1 из массива  friends
+
+//10. поменяйте объекту с id=2 из массива  friends значение св-ва name на
+// "Donald"
+
+//11. добавьте в список друзей нового друга
+
+// И поднимаем руку!!!!
+
+//12. Найдите студента с самым высоким баллом не используя методы массивов и
+// Math.max()*
+
+
+//13. Найдите сумму баллов всех студентов (reduce)*
+
+// «arr.reduce(callback[, initialValue])» 
+//callback(previousValue, currentItem, index, arr):
+
+// 14. Д.З.:
+// Напишите функцию addFriends, которая принимает параметром массив students
+// и добавляет в каждому студенту свойство "friends",
+// значением которого является массив имён всех остальных студентов из массива students,
+// за исключением собственного имени студента. Т.е. в друзьях у Боба Боба быть не должно.
+
+
+// 15. Д.З.: Напишите функцию getBestStudents, которая принимает параметром
+// массив students  и количество лучших студентов, которое надо получить в
+// новом массиве. Если второго параметра нет, то по умолчанию возвращает лучшего студента
+// getBestStudents(students) => {name: "Nick", age: 20, isMarried: false, scores: 120}
+// getBestStudents(students, 3) => [{...}, {...}, {...}]
+// getBestStudents(students, 10) => [{}, {}, ...., {}, null, null, null, null ]
