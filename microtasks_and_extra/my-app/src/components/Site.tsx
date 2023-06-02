3d3f862daa661df6e81aa673420dbaefbbd5638c
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-06-02 19:11:42 +0300

    implement appears and disappears nav menu

--- microtasks_and_extra/my-app/src/components/Site.tsx
+++ microtasks_and_extra/my-app/src/components/Site.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './Site.module.css'
 import {Navigate, NavLink, Route, Routes, useParams} from "react-router-dom";
 import {PageOne} from "./pages/PageOne";
@@ -8,24 +8,25 @@ import {Error404} from "./pages/Error404";
 import {Page} from "./pages/Page";
 import {dataState} from "../dataState/dataState";
 import {S} from './pages/Page_styles'
+import {useWindowSize} from "../hooks/useWindowSize";
 
 
 export const Site = () => {
 
-  const [windowSize, setWindowSize] = useState()
+  const size = useWindowSize() // count window width
 
   return (
     <div>
       <div className={styles.header}><h1>HEADER</h1></div>
       <div className={styles.body}>
-        <div className={styles.nav}>
+        {size > 1000 && <div className={styles.nav}>
           <S.NavWrapper><NavLink to={'/page/0'}>PAGE
             ONE</NavLink></S.NavWrapper>
           <S.NavWrapper><NavLink to={'/page/1'}>PAGE
             TWO</NavLink></S.NavWrapper>
           <S.NavWrapper><NavLink to={'/page/2'}>PAGE
             THREE</NavLink></S.NavWrapper>
-        </div>
+        </div>}
         <div className={styles.content}>
           <Routes>
             <Route path={'/'} element={<Navigate to={'/page/0'}/>}/>
