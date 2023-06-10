6b5a23e607907bd9c528c55132ee16fcdd7d33d3
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-06-10 15:40:54 +0300

    solve medium level of task

--- microtasks_and_extra/my-app/src/App.tsx
+++ microtasks_and_extra/my-app/src/App.tsx
@@ -2,113 +2,120 @@
 // Начальная src
 
 // // EASY LEVEL
+// import React, { useState } from 'react'
+// import './App.css'
+// import { WishList } from './WishList'
+// import { v1 } from 'uuid'
+//
+// function App() {
+//   let [tasks, setTasks] = useState([
+//     { id: v1(), title: 'HTML&CSS', isDone: true },
+//     { id: v1(), title: 'JS', isDone: true },
+//     { id: v1(), title: 'ReactJS', isDone: false },
+//     { id: v1(), title: 'Rest API', isDone: false },
+//     { id: v1(), title: 'GraphQL', isDone: false },
+//   ])
+//
+//   function removeTask(taskID: string) {
+//     setTasks(tasks.filter(item => item.id !== taskID))
+//   }
+//
+//   function changeTaskStatus(taskId: string, isDone: boolean) {
+//     setTasks(tasks.map(item => (item.id === taskId ? { ...item, isDone } : item)))
+//   }
+//
+//   return (
+//     <div className="App">
+//       <WishList
+//         title="What to learn"
+//         tasks={tasks}
+//         removeTask={removeTask}
+//         changeTaskStatus={changeTaskStatus}
+//       />
+//     </div>
+//   )
+// }
+//
+// export default App
+
+// // MEDIUM LEVEL
+
 import React, { useState } from 'react'
 import './App.css'
-import { WishList } from './WishList'
+import { TaskType, WishList } from './WishList'
 import { v1 } from 'uuid'
 
+export type FilterValuesType = 'all' | 'active' | 'completed'
+
+type TodolistsType = {
+  id: string
+  title: string
+  filter: FilterValuesType
+}
+type TasksType = {
+  [key: string]: Array<TaskType>
+}
+
 function App() {
-  let [tasks, setTasks] = useState([
-    { id: v1(), title: 'HTML&CSS', isDone: true },
-    { id: v1(), title: 'JS', isDone: true },
-    { id: v1(), title: 'ReactJS', isDone: false },
-    { id: v1(), title: 'Rest API', isDone: false },
-    { id: v1(), title: 'GraphQL', isDone: false },
+  let todolistID1 = v1()
+  let todolistID2 = v1()
+
+  let [todolists, setTodolists] = useState<Array<TodolistsType>>([
+    { id: todolistID1, title: 'What to learn', filter: 'all' },
+    { id: todolistID2, title: 'What to buy', filter: 'all' },
   ])
 
-  function removeTask(taskID: string) {
-    setTasks(tasks.filter(item => item.id !== taskID))
+  let [tasks, setTasks] = useState<TasksType>({
+    [todolistID1]: [
+      { id: v1(), title: 'HTML&CSS', isDone: true },
+      { id: v1(), title: 'JS', isDone: true },
+      { id: v1(), title: 'ReactJS', isDone: false },
+      { id: v1(), title: 'Rest API', isDone: false },
+      { id: v1(), title: 'Graph SQL', isDone: false },
+    ],
+    [todolistID2]: [
+      { id: v1(), title: 'Milk', isDone: true },
+      { id: v1(), title: 'Fruits', isDone: true },
+      { id: v1(), title: 'Nuts', isDone: true },
+      { id: v1(), title: 'Bread', isDone: false },
+      { id: v1(), title: 'Sugar', isDone: false },
+    ],
+  })
+
+  function removeTask(todolistID: string, taskID: string) {
+    setTasks({ ...tasks, [todolistID]: tasks[todolistID].filter(item => item.id !== taskID) })
   }
 
-  function changeTaskStatus(taskId: string, isDone: boolean) {
-    setTasks(tasks.map(item => (item.id === taskId ? { ...item, isDone } : item)))
+  function changeStatus(todolistID: string, taskId: string, isDone: boolean) {
+    setTasks({
+      ...tasks,
+      [todolistID]: tasks[todolistID].map(item =>
+        item.id === taskId ? { ...item, isDone } : item
+      ),
+    })
   }
 
   return (
     <div className="App">
-      <WishList
-        title="What to learn"
-        tasks={tasks}
-        removeTask={removeTask}
-        changeTaskStatus={changeTaskStatus}
-      />
+      {todolists.map(el => {
+        let tasksForTodolist = tasks[el.id]
+
+        return (
+          <WishList
+            key={el.id}
+            todolistID={el.id}
+            title={el.title}
+            tasks={tasksForTodolist}
+            removeTask={removeTask}
+            changeTaskStatus={changeStatus}
+          />
+        )
+      })}
     </div>
   )
 }
 
 export default App
-
-// // MEDIUM LEVEL
-
-// import React, {useState} from 'react';
-// import './App.css';
-// import {TaskType, WishList} from './WishList';
-// import {v1} from 'uuid';
-//
-// export type FilterValuesType = "all" | "active" | "completed";
-//
-// type TodolistsType = {
-//   id: string;
-//   title: string;
-//   filter: FilterValuesType;
-// }
-// type TasksType = {
-//   [key: string]: Array<TaskType>
-// }
-//
-// function App() {
-//   let todolistID1 = v1()
-//   let todolistID2 = v1()
-//
-//   let [todolists, setTodolists] = useState<Array<TodolistsType>>([
-//     {id: todolistID1, title: "What to learn", filter: "all"},
-//     {id: todolistID2, title: "What to buy", filter: "all"},
-//   ])
-//
-//   let [tasks, setTasks] = useState<TasksType>({
-//     [todolistID1]: [
-//       {id: v1(), title: 'HTML&CSS', isDone: true},
-//       {id: v1(), title: 'JS', isDone: true},
-//       {id: v1(), title: 'ReactJS', isDone: false},
-//       {id: v1(), title: 'Rest API', isDone: false},
-//       {id: v1(), title: 'Graph SQL', isDone: false},
-//
-//     ],
-//     [todolistID2]: [
-//       {id: v1(), title: 'Milk', isDone: true},
-//       {id: v1(), title: 'Fruits', isDone: true},
-//       {id: v1(), title: 'Nuts', isDone: true},
-//       {id: v1(), title: 'Bread', isDone: false},
-//       {id: v1(), title: 'Sugar', isDone: false},
-//     ]
-//   })
-//
-//   function removeTask(todolistID: string, taskID: string) {
-//     //setTasks();
-//   }
-//
-//   function changeStatus(todolistID: string, taskId: string, isDone: boolean) {
-//     //setTasks()
-//   }
-//
-//   return (
-//       <div className="App">
-//         {todolists.map(el => {
-//           let tasksForTodolist = tasks[el.id];
-//           return <WishList
-//               key={el.id}
-//               todolistID={el.id}
-//               title={el.title}
-//               tasks={tasksForTodolist}
-//               removeTask={removeTask}
-//               changeTaskStatus={changeStatus}
-//           />
-//         })}
-//       </div>
-//   );
-// }
-//
-// export default App;
 
 // // HARD LEVEL
 
--- microtasks_and_extra/my-app/src/WishList.tsx
+++ microtasks_and_extra/my-app/src/WishList.tsx
@@ -2,35 +2,78 @@
 // Начальная src
 
 // EASY LEVEL
+// import React, { ChangeEvent } from 'react'
+//
+// type TaskType = {
+//   id: string
+//   title: string
+//   isDone: boolean
+// }
+//
+// type PropsType = {
+//   title: string
+//   tasks: Array<TaskType>
+//   removeTask: (taskId: string) => void
+//   changeTaskStatus: (taskId: string, isDone: boolean) => void
+// }
+//
+// export function WishList(props: PropsType) {
+//   return (
+//     <div>
+//       <h3>{props.title}</h3>
+//       <ul>
+//         {props.tasks.map(t => {
+//           const onClickHandler = () => props.removeTask(t.id)
+//           const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+//             props.changeTaskStatus(t.id, e.currentTarget.checked)
+//           }
+//
+//           return (
+//             <li key={t.id} className={t.isDone ? 'is-done' : ''}>
+//               <input type="checkbox" checked={t.isDone} onChange={onChangeHandler} />
+//               <span>{t.title}</span>
+//               <button onClick={onClickHandler}>x</button>
+//             </li>
+//           )
+//         })}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// // MEDIUM LEVEL and HARD LEVEL
+//
 import React, { ChangeEvent } from 'react'
 
-type TaskType = {
+export type TaskType = {
   id: string
   title: string
   isDone: boolean
 }
 
 type PropsType = {
+  todolistID: string
   title: string
   tasks: Array<TaskType>
-  removeTask: (taskId: string) => void
-  changeTaskStatus: (taskId: string, isDone: boolean) => void
+  removeTask: (todolistID: string, taskID: string) => void
+  changeTaskStatus: (todolistID: string, taskId: string, isDone: boolean) => void
 }
 
 export function WishList(props: PropsType) {
   return (
     <div>
       <h3>{props.title}</h3>
+
       <ul>
         {props.tasks.map(t => {
-          const onClickHandler = () => props.removeTask(t.id)
+          const onClickHandler = () => props.removeTask(props.todolistID, t.id)
           const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-            props.changeTaskStatus(t.id, e.currentTarget.checked)
+            props.changeTaskStatus(props.todolistID, t.id, e.currentTarget.checked)
           }
 
           return (
             <li key={t.id} className={t.isDone ? 'is-done' : ''}>
-              <input type="checkbox" checked={t.isDone} onChange={onChangeHandler} />
+              <input type="checkbox" onChange={onChangeHandler} checked={t.isDone} />
               <span>{t.title}</span>
               <button onClick={onClickHandler}>x</button>
             </li>
@@ -40,51 +83,6 @@ export function WishList(props: PropsType) {
     </div>
   )
 }
-
-// // MEDIUM LEVEL and HARD LEVEL
-//
-// import React, {ChangeEvent} from 'react';
-//
-// export type TaskType = {
-// 	id: string
-// 	title: string
-// 	isDone: boolean
-// }
-//
-// type PropsType = {
-// 	todolistID: string
-// 	title: string
-// 	tasks: Array<TaskType>
-// 	removeTask: (todolistID: string, taskID: string) => void
-// 	changeTaskStatus: (todolistID: string, taskId: string, isDone: boolean) => void
-// }
-//
-// export function WishList(props: PropsType) {
-// 	return <div>
-// 		<h3>
-// 			{props.title}
-// 		</h3>
-//
-// 		<ul>
-// 			{
-// 				props.tasks.map(t => {
-// 					const onClickHandler = () => props.removeTask(props.todolistID, t.id)
-// 					const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-// 						props.changeTaskStatus(props.todolistID, t.id, e.currentTarget.checked);
-// 					}
-//
-// 					return <li key={t.id} className={t.isDone ? "is-done" : ""}>
-// 						<input type="checkbox"
-// 							   onChange={onChangeHandler}
-// 							   checked={t.isDone}/>
-// 						<span>{t.title}</span>
-// 						<button onClick={onClickHandler}>x</button>
-// 					</li>
-// 				})
-// 			}
-// 		</ul>
-// 	</div>
-// }
 
 // Илья 02.06.2023
 // Начальная src
