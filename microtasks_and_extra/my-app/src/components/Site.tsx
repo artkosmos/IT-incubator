111b220bab7adebb3a87555bedc59b97cfd6d14c
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-06-19 11:12:46 +0300

    implement Routes and NavLinks

--- microtasks_and_extra/my-app/src/components/Site.tsx
+++ microtasks_and_extra/my-app/src/components/Site.tsx
@@ -6,25 +6,40 @@ import {Error404} from "./pages/Error404";
 import {PageOne} from "./pages/PageOne";
 import {PageTwo} from "./pages/PageTwo";
 import {PageThree} from "./pages/PageThree";
+import {Route, Routes, Navigate, NavLink} from "react-router-dom";
 export const Site = () => {
+
+    const activeClass = (isActive:boolean) => isActive ? styles.active : styles.navLink
+
     return (
         <div>
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    Links
+                    <div><NavLink
+                        to={'/page/1'}
+                        className={({isActive}) => activeClass(isActive)}
+                    >Page One
+                    </NavLink></div>
+                    <div><NavLink
+                        to={'/page/2'}
+                        className={({isActive}) => activeClass(isActive)}
+                    >Page Two
+                    </NavLink></div>
+                    <div><NavLink
+                        to={'/page/3'}
+                        className={({isActive}) => activeClass(isActive)}
+                    >Page Three
+                    </NavLink></div>
                 </div>
                 <div className={styles.content}>
-
-                            Content
-
-                    {/*<PageOne/>*/}
-                    {/*<PageTwo/>*/}
-                    {/*<PageThree/>*/}
-                    {/*<Error404/>*/}
-
-                    {/*<-------------->*/}
-
+                <Routes>
+                    <Route path={'/'} element={<Navigate to={'/page1'}/>}/>
+                    <Route path={'/page1'} element={<PageOne/>}/>
+                    <Route path={'/page2'} element={<PageTwo/>}/>
+                    <Route path={'/page3'} element={<PageThree/>}/>
+                    <Route path={'/error'} element={<Error404/>}/>
+                </Routes>
                     {/*<HomePage/>*/}
                     {/*<Content/>*/}
                     {/*<Error404/>*/}
