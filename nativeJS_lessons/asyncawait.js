c32d8f7d568b1ef731503dbc14d82b0c997343cf
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-07-12 18:48:10 +0300

    add async await training

---
+++ nativeJS_lessons/asyncawait.js
@@ -0,0 +1,166 @@
+
+//============================== async/await ================================
+
+const foo1 = () => { // стандартная запись создания промиса resolve
+  return new Promise((res) => {
+    res(123)
+  })
+}
+
+const foo = async () => { // сразу резолвит и возвращает промис (короткая запись)
+  return 123
+}
+
+// еще 2 вида записи:
+// const foo2 = async function() {}
+// async function() {}
+
+const timerPromise = () => {  // промис зарезолвится через 2 секунды
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(2)
+    }, 2000)
+  })
+}
+
+const asyncFn = async () => {
+  console.log('timer start')
+  await timerPromise()  // подождать пока выполнится промис (типо синхронная функция)
+  console.log('timer end') // выполнится только после результата выполнения await
+}
+
+asyncFn()
+console.log(1) // выполнится после первого вывода в консоль, так как код выполняется синхронно, пропуская await
+
+//============================== Задание ================================
+/*
+1. Отправь запрос с методом get для получения пользователей, используя метод fetch
+const USERS_URL = `https://jsonplaceholder.typicode.com/users`
+
+2. Обработай ответ response и декодируй его с помощью метода (какого) ????
+3. Выведи в консоль users
+console.log('users', users)
+4. Обработай возможную ошибку и в случае ее возникновения: выведи error
+console.log('error', error)
+
+5.Выведи в консоль первого пользователя (с id=1)
+ console.log('firstUserId', firstUserId)
+6. Получи весь список задач (todos) для первого пользователя (с id=1)
+Подумай где это нужно делать?
+
+Базовый URL:
+const TODOS_URL = `https://jsonplaceholder.typicode.com/todos`
+
+Пример url:
+const USERS_URL = `https://jsonplaceholder.typicode.com/todos?userId=1`
+
+Подсказка: нужно отправить новый запрос, используя данные полученные из предыдущего запроса
+7. Выведи данные задачи в консоль и проделай все по аналогии с пользователями
+(декодирование, обработка ошибки и тд):
+console.log('todos', todos)
+
+Проверь полученный результат (везде дб userId=1)
+
+8. Получается неудобный код (если делать запросы снова -> растет вправо)
+-> перепиши код на async/await
+
+Для этого создай функцию getTodosWithUserData
+
+9. Вызови результат функции в консоль
+console.log('promise', promise)
+
+10. Обработай ошибки и напиши какой-нибудь finally
+11. Проверь работоспособность catch
+12. Попробуй сломать URL -> посмотри что будет
+ */
+
+const TODOS_URL = `https://jsonplaceholder.typicode.com/todos`
+const USERS_URL = `https://jsonplaceholder.typicode.com/users`
+
+fetch(USERS_URL)
+  .then(response => response.json())
+  .then(users => {
+    console.log('users', users)
+    const firstUser = users[0].id // id первого user
+
+    fetch(`${TODOS_URL}?userId=${firstUser}`)
+      .then(response => response.json()) // декодирование данных
+      .then(todos => console.log(todos))
+  })
+  .catch(error => {
+
+    console.log('error', error)
+  })
+// переписать коды выше на async await
+const fooAsync = async () => {
+  try {
+    const usersResponse = await fetch(USERS_URL)
+    const users = await usersResponse.json()
+    const firstUser = users[0].id
+
+    const todoResponse = await fetch(`${TODOS_URL}?userId=${firstUser}`)
+    const todoForFirstUser = await todoResponse.json()
+
+    return todoForFirstUser
+
+  } catch (error) {
+    console.log('error', error)
+  }
+}
+fooAsync().then(data => console.log(data) )
+
+//============================== Задачи ================================
+
+//============================== Задача 1 ================================
+
+/*
+Требуется переписать данный код, который использует then, catch и finally, в код,
+который использует исключительно async await и try, catch, finally.
+
+
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+let isLoading = false;
+const createNewPost = () => {
+    isLoading = true;
+    fetch(POSTS_URL, {
+        method: 'POST',
+    })
+        .then((response) => response.json())
+        .then((result) => {
+            console.log('result', result)
+        })
+        .catch((error) => {
+            console.log('error', error)
+        })
+        .finally(() => {
+            isLoading = false;
+        });
+};
+createNewPost();
+ */
+
+
+
+//============================== Задача 2 ================================
+/*
+Требуется переписать данный код, который использует then и catch, в код, который
+использует исключительно async await и try, catch.
+
+const TODOS_URL = 'https://jsonplaceholder.typicode.com/todos';
+const getTodosByIds = (ids) => {
+const requests = ids.map((id) => fetch(`${TODOS_URL}/${id}`));
+Promise.all(requests)
+.then((responses) => {
+const dataResults = responses.map((data) => data.json());
+return Promise.all(dataResults)
+})
+.then((allTasks) => {
+console.log(allTasks);
+})
+.catch((error) => {
+console.log(error);
+})
+}
+getTodosByIds([43, 21, 55, 100, 10]);
+ */
+
