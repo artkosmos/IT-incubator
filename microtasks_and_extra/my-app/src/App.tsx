47b35c7da139802f622d814ca2e4eed104081569
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-07-13 13:48:07 +0300

    start wednesday additional lesson's first task

--- microtasks_and_extra/my-app/src/App.tsx
+++ microtasks_and_extra/my-app/src/App.tsx
@@ -1,56 +1,24 @@
-import React, {Fragment, useCallback, useEffect, useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {selectUsers, selectUsersCount} from 'store/selectors';
-import {createUser, plusUserCount} from 'store/actions';
-import {v1} from 'uuid';
-import {CustomButton, UsersCountValue} from 'components';
-import {UsersList} from 'components/UsersList';
-import {generateRandomName} from 'utils';
-import {SetTimeoutType} from 'types';
+import {Fragment} from 'react';
 
-const DELAY = 500;
+import {Task_1} from "./task_1/Task_1";
+
+export type UserType = {
+  [key: string]: { name: string, id: string }
+};
+
+const DATA_7: UserType[] = [
+  {u1: {name: 'Gleb', id: '1'}},
+  {u2: {name: 'Dima', id: '2'}},
+  {u3: {name: 'Svetlana', id: '3'}},
+  {u4: {name: 'Artur', id: '4'}},
+  {u5: {name: 'Vera', id: '5'}},
+  {u6: {name: 'Vlad', id: '6'}},
+];
 
 export const App = () => {
-  const dispatch = useDispatch();
-
-  const users = useSelector(selectUsers);
-  const usersCount = useSelector(selectUsersCount);
-  console.log(users)
-  console.log(usersCount)
-
-  const [isAddNewUser, setIsAddNewUser] = useState<boolean>(false);
-
-  useEffect(() => {
-    let timeoutId: SetTimeoutType = setTimeout((): void => {
-      setIsAddNewUser(false);
-    }, DELAY);
-
-    return () => clearTimeout(timeoutId);
-  }, [users.length]);
-
-  useEffect(() => {
-    if (users.length > 0 && isAddNewUser) {
-      dispatch(plusUserCount());
-    }
-  }, [dispatch, users.length, isAddNewUser]);
-
-  const handleClick = useCallback((): void => {
-    dispatch(createUser({id: v1(), name: generateRandomName()}));
-
-    setIsAddNewUser(true);
-  }, [dispatch]);
-
   return (
     <Fragment>
-      <UsersCountValue usersCount={usersCount} />
-
-      <CustomButton
-        title="Click Me"
-        isButtonDisabled={isAddNewUser}
-        onClick={handleClick}
-      />
-
-      <UsersList users={users} />
+      <Task_1 />
     </Fragment>
   );
 };
