79d6b9312065ac3867c5f97e27fd9d77a20c73c0
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-07-13 15:20:17 +0300

    task was solved

--- microtasks_and_extra/my-app/src/task_1/Task_1.tsx
+++ microtasks_and_extra/my-app/src/task_1/Task_1.tsx
@@ -1,32 +1,35 @@
-import {ChangeEvent, FC, useCallback, useState} from 'react';
+import {ChangeEvent, memo, useState} from 'react';
 
 const CONTAINER_STYLES = {display: 'flex', flexDirection: 'column', gap: 10};
 
-type PropsType = { value: string, onChange: (e: ChangeEvent<HTMLInputElement>) => void };
+// type PropsType = { value: string, onChange: (e: ChangeEvent<HTMLInputElement>) => void };
 
 // Task 1
 // If change value on input Title component not re-render
 // If change value on input Task_1 component not re-render
 
 export const Task_1 = () => {
-  const [value, setValue] = useState('');
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.currentTarget.value);
-  };
 
   return (
     <div style={{...CONTAINER_STYLES} as any}>
-      <Input value={value} onChange={handleChange} />
+      <Input/>
       <Title title="I am a title" />
     </div>
-  );
-};
+  )
+}
 
-const Title = (props: { title: string }) => <h3>{props.title}</h3>;
-
-const Input: FC<PropsType> = ({value, onChange}) => {
+const Title = memo((props: { title: string }) => {
   return (
-    <input type="text" placeholder="Placeholder" value={value} onChange={onChange} />
-  );
-};
+    <h3>{props.title}</h3>
+  )
+})
+
+const Input = () => {
+  const [value, setValue] = useState('')
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value);
+  }
+  return (
+    <input type="text" placeholder="Placeholder" value={value} onChange={handleChange} />
+  )
+}
